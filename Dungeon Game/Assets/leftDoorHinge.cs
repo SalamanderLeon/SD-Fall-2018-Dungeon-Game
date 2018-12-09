a04e872c85b30345ae80fa8b8f8dae3d4335e9fd
YangYu <1820201@AIV.EDMC.STU>
2018-12-08 19:51:43 -0800

    Fixed door but task system data lost

---
+++ Dungeon Game/Assets/leftDoorHinge.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftDoorHinge : MonoBehaviour {
+
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(0, 90, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            iTweenEvent.GetEvent(gameObject, "DoorOpen").Play();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            iTweenEvent.GetEvent(gameObject, "DoorClose").Play();
+        }
+    }
+}
